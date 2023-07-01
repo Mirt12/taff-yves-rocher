cfeca23d2b49d2ce902c9e24e6215a5a9c302a4a
Mirt12 <Mirt12@yandex.ru>
2023-07-01 16:47:34 +0200

    add isTopSellingProductsTwentyTest to HomePageRest tests

--- src/test/java/by/itacademy/tatjana/balashevich/api/RestApiHomePageTest.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/RestApiHomePageTest.java
@@ -1,6 +1,9 @@
 package by.itacademy.tatjana.balashevich.api;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
 import static io.restassured.RestAssured.given;
 
@@ -23,4 +26,12 @@ public class RestApiHomePageTest {
                 assertThat().statusCode(200);
     }
 
+    @Test
+    public void isTopSellingProductsTwentyTest() {
+        Response response = given().header("accept", "application/json").
+                            when().get("https://api.y-r.by/api/v1/products?filter[is_top_seller]=1&limit=20");
+        JsonPath jsonPath = response.jsonPath();
+        int jsonSize = jsonPath.getInt("data.size()");
+        Assert.assertEquals(jsonSize, 20);
+    }
 }
