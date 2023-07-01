c132e41ab4916dd6a6fde75445f3337137459562
Mirt12 <Mirt12@yandex.ru>
2023-07-01 15:34:29 +0200

    add HomePage Rest tests

--- src/test/java/by/itacademy/tatjana/balashevich/api/RestApiHomePageTest.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/RestApiHomePageTest.java
@@ -1,4 +1,26 @@
 package by.itacademy.tatjana.balashevich.api;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class RestApiHomePageTest {
+    @Test
+    public void loadHomePageHtmlTest() {
+        String url = "https://www.y-r.by";
+        given().header("accept", "application/json").
+                when().get(url).
+                then().log().body().
+                assertThat().statusCode(200);
+    }
+
+    @Test
+    public void loadTopSellingProductsTest() {
+        String url = "https://api.y-r.by/api/v1/products?filter[is_top_seller]=1&limit=20";
+        given().header("accept", "application/json").
+                when().get(url).
+                then().log().body().
+                assertThat().statusCode(200);
+    }
+
 }
