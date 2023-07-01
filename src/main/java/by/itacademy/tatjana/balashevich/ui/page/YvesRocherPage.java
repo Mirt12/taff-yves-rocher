2ffb4f38daafbcaf1c31cca61b7e9d19ed6a10d5
Mirt12 <Mirt12@yandex.ru>
2023-07-01 21:22:38 +0200

    add YR Page locators

--- src/main/java/by/itacademy/tatjana/balashevich/ui/page/YvesRocherPage.java
+++ src/main/java/by/itacademy/tatjana/balashevich/ui/page/YvesRocherPage.java
@@ -1,5 +1,12 @@
 package by.itacademy.tatjana.balashevich.ui.page;
 
 public class YvesRocherPage {
+    String url = "https://www.y-r.by";
+    String homePageCloseLanguageModalLocator = "//button[@aria-label='Close']";
+    String homePageLoginLinkLocator = "//a[@href='/auth/login']";
+    String authorizationLinkLocator = "//h2[contains(text(),'Авторизация')]";
+    String loginFormInputEmailLocator = "//input[@id='user-email']";
+    String loginFormInputPwdLocator = "//input[@id='user-password']";
+    String submitLoginFormButtonLocator = "//button[contains(text(),'Авторизация')]";
 
 }
