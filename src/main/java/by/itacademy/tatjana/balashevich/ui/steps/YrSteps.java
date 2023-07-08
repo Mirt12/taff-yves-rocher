831bc130435ce7980057fa5770f3950e1cc9e3a6
Mirt12 <Mirt12@yandex.ru>
2023-07-08 19:40:15 +0200

    add YrSteps class

---
+++ src/main/java/by/itacademy/tatjana/balashevich/ui/steps/YrSteps.java
@@ -0,0 +1,23 @@
+package by.itacademy.tatjana.balashevich.ui.steps;
+
+import by.itacademy.tatjana.balashevich.ui.page.YvesRocherPage;
+import by.itacademy.tatjana.balashevich.ui.utils.LoadHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+
+public class YrSteps {
+
+    WebDriver driver;
+
+    public YrSteps(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void fillLoginFormAndSubmit(String email, String password) {
+        YvesRocherPage yvesRocherPage = new YvesRocherPage(driver);
+        Wait<WebDriver> wait = LoadHelper.wait30seconds(driver);
+        yvesRocherPage.fillInputFieldEmail(wait, email);
+        yvesRocherPage.fillInputFieldPassword(password);
+        yvesRocherPage.clickSubmitButtonOfLoginForm();
+    }
+}
