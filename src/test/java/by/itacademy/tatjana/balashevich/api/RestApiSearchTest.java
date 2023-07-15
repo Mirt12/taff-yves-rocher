0447eae8e19e97bb2f4999e0549d09e87ab84c6f
Mirt12 <tanyabalashevich@gmail.com>
2023-07-15 19:28:38 +0200

    code refactoring of RestApiSearchTest class

--- src/test/java/by/itacademy/tatjana/balashevich/api/RestApiSearchTest.java
+++ src/test/java/by/itacademy/tatjana/balashevich/api/RestApiSearchTest.java
@@ -12,8 +12,8 @@ import static org.hamcrest.Matchers.*;
 
 public class RestApiSearchTest {
     @Test
-    public void hasSearchProductTitleKey() {
-        String url = "https://api.y-r.by/api/v1/products?search=����";
+    public void hasSearchProductTitleKeyTest() {
+        String url = "https://api.y-r.by/api/v1/products?search=крем";
         given().header("accept", "application/json").
                 when().get(url).
                 then().
@@ -22,20 +22,20 @@ public class RestApiSearchTest {
     }
 
     @Test
-    public void hasSearchProductCremeValue() {
-        String url = "https://api.y-r.by/api/v1/products?search=����";
+    public void hasSearchProductCremeValueTest() {
+        String url = "https://api.y-r.by/api/v1/products?search=крем";
         given().header("accept", "application/json").
                 when().get(url).
                 then().
                 assertThat().statusCode(200).
-                assertThat().body("data[0].category", containsString("����"));
+                assertThat().body("data[0].title", containsString("Крем"));
     }
 
     @Test
-    public void isInvalidSearchHasEmptyArray() {
+    public void isInvalidSearchHasEmptyArrayTest() {
         //to save response
         Response response = given().header("accept", "application/json").
-                when().get("https://api.y-r.by/api/v1/products?search=�������");
+                when().get("https://api.y-r.by/api/v1/products?search=антарес");
         //to save json body
         JsonPath jsonPath = response.jsonPath();
         int jsonSize = jsonPath.getInt("data.size()");
