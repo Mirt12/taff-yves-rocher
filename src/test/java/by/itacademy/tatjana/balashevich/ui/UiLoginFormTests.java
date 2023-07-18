55e62c6020c16ccf936d1c1f6ce0fb735eb2de30
Mirt12 <tanyabalashevich@gmail.com>
2023-07-18 19:46:47 +0200

    refactoring of UILoginTests class

--- src/test/java/by/itacademy/tatjana/balashevich/ui/UiLoginFormTests.java
+++ src/test/java/by/itacademy/tatjana/balashevich/ui/UiLoginFormTests.java
@@ -11,11 +11,9 @@ import java.io.UnsupportedEncodingException;
 
 public class UiLoginFormTests extends BaseUITest {
 
-    YrSteps yrSteps;
-
     @Test
     public void userCanOpenLoginForm()throws UnsupportedEncodingException, InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.openLoginForm();
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.authorizationHeaderLocator);
         Assertions.assertEquals(YvesRocherPage.expectedAuthorizationHeaderText, actualText);
@@ -23,7 +21,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void toFillLoginFormByCorrectDataTest() throws UnsupportedEncodingException, InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit("tbalashevich@bk.ru", "PostinG@2579!");
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.loginGreetingLocator);
         Assertions.assertEquals(YvesRocherPage.expectedHeaderText, actualText);
@@ -31,7 +29,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void toFillLoginFormByEmptyEmailTest() throws InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit("", "PostinG@2579!");
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.emailErrorLocator);
         Assertions.assertEquals(YvesRocherPage.expectedLoginErrorText, actualText);
@@ -39,7 +37,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void toFillLoginFormByEmptyPasswordTest() throws InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit("tbalashevich@bk.ru", "");
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.passwordErrorLocator);
         Assertions.assertEquals(YvesRocherPage.expectedLoginErrorText, actualText);
@@ -47,7 +45,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void invalidEmailErrorTest() throws InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit("@@@", "PostinG@2579!");
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.emailErrorLocator);
         Assertions.assertEquals(YvesRocherPage.expectedEmailErrorText, actualText);
@@ -55,7 +53,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void loginErrorForNotCreatedUserTest() throws InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit(Util.generateEmail(), Util.generatePWD());
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.errorLocatorForNotExistUser);
         Assertions.assertEquals(YvesRocherPage.expectedErrorTextForNotExistUser, actualText);
@@ -63,7 +61,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void shortPasswordErrorTest() throws InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit(Util.generateEmail(), "88888");
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.passwordErrorLocator);
         Assertions.assertEquals(YvesRocherPage.expectedErrorTextForShortPwd, actualText);
@@ -71,7 +69,7 @@ public class UiLoginFormTests extends BaseUITest {
 
     @Test
     public void longPasswordErrorTest() throws InterruptedException {
-        yrSteps = new YrSteps(driver);
+        YrSteps yrSteps = new YrSteps(driver);
         yrSteps.fillLoginFormAndSubmit(Util.generateEmail(), "8888888888888888888888888888888");
         String actualText = LoadHelper.getTextByLocator(driver, YvesRocherPage.passwordErrorLocator);
         Assertions.assertEquals(YvesRocherPage.expectedErrorTextForLongPwd, actualText);
