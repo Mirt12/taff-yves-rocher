c13764874f758084668a496d22e8787de3556dc8
Mirt12 <tanyabalashevich@gmail.com>
2023-07-18 19:05:02 +0200

    refactoring of imports

--- src/test/java/by/itacademy/tatjana/balashevich/ui/BaseUITest.java
+++ src/test/java/by/itacademy/tatjana/balashevich/ui/BaseUITest.java
@@ -1,6 +1,5 @@
 package by.itacademy.tatjana.balashevich.ui;
 
-import by.itacademy.tatjana.balashevich.ui.steps.YrSteps;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
