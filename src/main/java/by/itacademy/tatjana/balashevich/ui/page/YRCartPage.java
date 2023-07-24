c75077e1565a52d57a4b4112c103eb8a3f4db91b
Mirt12 <tanyabalashevich@gmail.com>
2023-07-24 14:43:45 +0200

    add PageObject for cart ui tests

--- src/main/java/by/itacademy/tatjana/balashevich/ui/page/YRCartPage.java
+++ src/main/java/by/itacademy/tatjana/balashevich/ui/page/YRCartPage.java
@@ -8,29 +8,31 @@ import org.openqa.selenium.support.ui.Wait;
 
 import java.util.function.Function;
 
-public class YRSearchPage {
+public class YRCartPage {
     private WebDriver driver;
-    private String url = "https://www.y-r.by";
+    private String baseUrl = "https://www.y-r.by";
     private String inputSearchLocator = "//input[@placeholder='Поиск']";
     private String btnCloseLanguageModalLocator = "//ngx-smart-modal//button[@type='button']";
     private String searchBtnLocator = "//button[@aria-label='search-btn']";
-    public static final String headerLocator = "//h1";
-    public static final String afterSearchTextLocator = "//div [@_ngcontent-serverapp-c86='']/p[@class='number-of-products ng-star-inserted']";
-    public static final String expectedHeaderText = "Поиск продукта";
-    public static final String expectedTextForInvalidSearch = "Поиск «антарес» не дал результатов. Попробуйте снова.";
+    private String addToCartBtnLocator = "//button[@class='basket-btn eye-button unselectable']";
 
 
-    public YRSearchPage() {
+    public static final String linkGoToCartLocator = "//a[@class='basket-btn unselectable eye-button']";
+    public static final String productLocatorInCart = "//a[@href='/parfyumeriya/parfyumernaya-voda-istinnyij-eliksir-pyorpl']";
+    public static final String expectedProductText = "Парфюмерная вода \"Истинный эликсир пёрпл\"";
+
+    public YRCartPage() {
         this.driver = DriverSingleton.getDriver();
     }
 
-    public YRSearchPage getUrl() {
-        driver.get(url);
+
+    public YRCartPage getUrl() {
+        driver.get(baseUrl);
         return this;
     }
 
 
-    public YRSearchPage closeLanguageModalOfHomePage(Wait wait) {
+    public YRCartPage closeLanguageModalOfHomePage(Wait wait) {
         WebElement btn = (WebElement) wait.until(new Function<WebDriver, WebElement>() {
             public WebElement apply(WebDriver driver) {
                 return driver.findElement(By.xpath(btnCloseLanguageModalLocator));
@@ -40,16 +42,28 @@ public class YRSearchPage {
         return this;
     }
 
-    public YRSearchPage fillSearchFieldByProduct(String searchWord) {
+    public YRCartPage fillSearchFieldByProduct(String searchWord) {
         WebElement searchInputField = driver.findElement(By.xpath(inputSearchLocator));
         searchInputField.sendKeys(searchWord);
         return this;
     }
 
 
-    public YRSearchPage clickSearchBtn() {
+    public YRCartPage clickSearchBtn() {
         WebElement searchBtn = driver.findElement(By.xpath(searchBtnLocator));
         searchBtn.click();
+        return this;
+    }
+
+    public YRCartPage clickAddToCartBtn(){
+        WebElement btn = driver.findElement(By.xpath(addToCartBtnLocator));
+        btn.click();
+        return this;
+    }
+
+    public YRCartPage clickLinkGoToCart(){
+        WebElement link = driver.findElement(By.xpath(linkGoToCartLocator));
+        link.click();
         return this;
     }
 }
